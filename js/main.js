151cc44023c548e7787ff0acbdc15ebf1779cff1
Zubair Ahmed <zubair1024@gmail.com>
2018-04-18 01:18:09 +0400

    CFD

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 (function ($) {
-  $(document).ready(function() {
-    $(window).on('resize', function(){
+  $(document).ready(function () {
+    $(window).on('resize', function () {
       window.screenWidth = $(window).width();
     }).resize();
     function getDuration() {
-      if(screenWidth > 768){      
+      if (screenWidth > 768) {
         return 4;
-      }else{
+      } else {
         return 1;
       }
     }
-    if($('#calendar').length > 0) {
+    if ($('#calendar').length > 0) {
       $('#calendar').fullCalendar({
         defaultView: 'basicWeek',
         startDate: new Date(),
-        defaultDate: new Date(new Date().getTime()+(2*24*60*60*1000)),
+        defaultDate: new Date(new Date().getTime() + (2 * 24 * 60 * 60 * 1000)),
         views: {
           agendaFourDays: {
             type: 'basicDay',
-            duration: { 
-              days: getDuration(),  
+            duration: {
+              days: getDuration(),
             },
             minTime: "10:00:00",
             maxTime: "22:00:00",
             dateIncrement: { days: 1 },
           }
-        }, 
-        nowIndicator : false,
+        },
+        nowIndicator: false,
         allDaySlot: false,
-        displayEventTime : false,
+        displayEventTime: false,
         slotDuration: '02:00:00',
         columnHeaderFormat: 'dddd D MMMM',
         weekends: false,
@@ -37,25 +37,25 @@
         events: {
           url: '/calendar-callbacks.php',
           type: 'POST',
-          data: { type: 'getPeriods'},
-          success: function(response) {
+          data: { type: 'getPeriods' },
+          success: function (response) {
           },
-          error: function() {
+          error: function () {
             alert('there was an error while fetching events!');
           },
         },
         eventColor: 'white',
         eventTextColor: '#231f1e',
         eventBorderColor: '#d9d9d9',
-        eventClick: function(calEvent, jsEvent, view) {
+        eventClick: function (calEvent, jsEvent, view) {
           var startDate = calEvent.start.format('YYYY-MM-DD HH:mm:ss');
-          var params = {type: 'bookAppointment', 'slot': startDate};
+          var params = { type: 'bookAppointment', 'slot': startDate };
           jQuery.ajax({
             type: "POST",
             url: "/calendar-callbacks.php",
             data: params,
             dataType: 'json',
-            success : function(response) {
+            success: function (response) {
               $('.book-appointment-btn').removeClass('disabled');
             }
           });
@@ -63,12 +63,12 @@
           $(this).css('border-color', '#df1514');
         },
         eventRender: function (event, element, view) {
-          $(element).css("margin", "0px 25px 5px 25px")
+          $(element).css("margin", "0px 25px 5px 25px");
         }
       });
-    }  
+    }
     // - Add class sticky on scroll
-    $(window).on( 'scroll', function(){
+    $(window).on('scroll', function () {
       if ($(window).scrollTop() > 150) {
         $('body').addClass('sticky');
       } else {
@@ -77,44 +77,44 @@
     });
     // $('body').css({'min-height': $(window).height()+392+'px'});
     // - Menu 
-    $('.menu-icon').click(function() {
-       $('.main-nav ').slideToggle(400,"swing"); 
+    $('.menu-icon').click(function () {
+      $('.main-nav ').slideToggle(400, "swing");
     });
     // - Quote page on load apply margin to allow scroll
-    if($('.quote-page').height() > $(window).height()) {
-      margin =  jQuery(window).height() - ($('.quote-page > div').last().height()+110+118);
+    if ($('.quote-page').height() > $(window).height()) {
+      margin = jQuery(window).height() - ($('.quote-page > div').last().height() + 110 + 118);
       jQuery('html,body').animate({
         scrollTop: $('.quote-page > div').last().offset().top - 118
       }, 1000);
-      jQuery('.quote-page').css({'margin-bottom': margin+'px'});
+      jQuery('.quote-page').css({ 'margin-bottom': margin + 'px' });
     }
     // - Book appointment disabled and enabled link
-    $('.book-appointment-btn').click(function(e){
-      if($(this).hasClass('disabled')){
+    $('.book-appointment-btn').click(function (e) {
+      if ($(this).hasClass('disabled')) {
         e.preventDefault();
       }
     });
     // - Add bootstrap classes to postcode lookup form
-    if($('#opc_input').length > 0) {
+    if ($('#opc_input').length > 0) {
       $('#opc_input').addClass('form-control');
-      $('#opc_button').addClass('btn'); 
-      $('#opc_button').click(function() {
+      $('#opc_button').addClass('btn');
+      $('#opc_button').click(function () {
         $('#opc_dropdown').addClass('form-control');
       });
     }
   });
 })(jQuery);
 
-function optSelect(num,opt,next,el) {
+function optSelect(num, opt, next, el) {
   jQuery(el).parent().find(jQuery('a')).removeClass("selected");
   jQuery(el).addClass("selected");
-	jQuery.ajax({
+  jQuery.ajax({
     type: "POST",
     url: "/callbacks.php",
     data: { type: 'questions', quesNum: num, optSelected: opt, nxt: next },
     dataType: 'json',
-    success : function(response) {
-      display(response.html, response.divInfo)   
+    success: function (response) {
+      display(response.html, response.divInfo)
     }
   });
 }
@@ -122,41 +122,41 @@ function optSelect(num,opt,next,el) {
 function serviceNotProvided(fuel) {
   swal({
     // animation: false,
-    title: 'Sorry', 
-    text: 'At this point in time we do not provide heating solution for '+fuel+' fuel.',
+    title: 'Sorry',
+    text: 'At this point in time we do not provide heating solution for ' + fuel + ' fuel.',
     confirmButtonText: 'Close'
   });
 }
-function display(html,divInfo) {
+function display(html, divInfo) {
   currentDiv = divInfo.quesNumber;
-  jQuery('#data-'+currentDiv).nextAll('.row').remove();
+  jQuery('#data-' + currentDiv).nextAll('.row').remove();
   jQuery('.main-container').append(html);
-  if(jQuery(window).height() > (jQuery('#'+divInfo.next).height()+110+118)) {
-    margin =  jQuery(window).height() - (jQuery('#'+divInfo.next).height()+110+118);
-  }else{
+  if (jQuery(window).height() > (jQuery('#' + divInfo.next).height() + 110 + 118)) {
+    margin = jQuery(window).height() - (jQuery('#' + divInfo.next).height() + 110 + 118);
+  } else {
     margin = 60
   }
   jQuery('html,body').animate({
-    scrollTop: jQuery('#'+divInfo.next).offset().top - 118
+    scrollTop: jQuery('#' + divInfo.next).offset().top - 118
   }, 600, 'swing');
-  jQuery('.main-container').css({'margin-bottom': margin+'px'});
+  jQuery('.main-container').css({ 'margin-bottom': margin + 'px' });
 }
 
 function getModel(el) {
   makeVal = jQuery(el).val();
-  jQuery('#model').attr('disabled','disabled');
-  if(makeVal == "") {
+  jQuery('#model').attr('disabled', 'disabled');
+  if (makeVal == "") {
     jQuery('#make').addClass('error');
     return;
-  }else{
+  } else {
     jQuery('#make').removeClass('error');
   }
   jQuery.ajax({
     type: "POST",
     url: "/callbacks.php",
-    data: { type: 'getModel', make: makeVal},
+    data: { type: 'getModel', make: makeVal },
     dataType: 'json',
-    success : function(response) {
+    success: function (response) {
       // console.log(response.selectModel); 
       jQuery('.model-wrapper').replaceWith(response.selectModel);
     }
@@ -166,21 +166,21 @@ function getModel(el) {
 function getQuote() {
   make = jQuery('#make').val();
   model = jQuery('#model').val();
-  if(make != '' && model != ''){
+  if (make != '' && model != '') {
     jQuery.ajax({
       type: "POST",
       url: "/callbacks.php",
-      data: { type: 'saveMakeModel', make: make, model: model},
+      data: { type: 'saveMakeModel', make: make, model: model },
       dataType: 'json',
-      success : function(response) {
+      success: function (response) {
         window.location.href = '/recommended-boilers';
       }
-    }); 
-  }else{
-    if(make == "") {
+    });
+  } else {
+    if (make == "") {
       jQuery('#make').addClass('error');
     }
-    if(model == "") {
+    if (model == "") {
       jQuery('#model').addClass('error');
     }
   }
@@ -190,11 +190,11 @@ function boilerSelected(id) {
   jQuery.ajax({
     type: "POST",
     url: "/callbacks.php",
-    data: { type: 'boilerSelected', pid: id},
+    data: { type: 'boilerSelected', pid: id },
     dataType: 'json',
-    success : function(response) {
-      if(response.msg == 'success'){ 
-        window.location.href = '/selected-boiler'; 
+    success: function (response) {
+      if (response.msg == 'success') {
+        window.location.href = '/selected-boiler';
       }
     }
   });
